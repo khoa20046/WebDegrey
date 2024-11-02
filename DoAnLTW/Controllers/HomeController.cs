d8f6ca8a1302680849ee34c7e4a66cce79209e5b
khoa20046 <foxg0381@gmail.com>
2024-11-02 12:18:27 +0700

    Add project files.

---
+++ DoAnLTW/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnLTW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+        public ActionResult SanPham()
+        {           
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ChiTietSanPham1()
+        {
+            return View();
+        }
+
+        public ActionResult ChiTietSanPham2()
+        {
+            return View();
+        }
+
+        public ActionResult ChiTietSanPham3()
+        {
+            return View();
+        }
+
+        public ActionResult ChiTietSanPham4()
+        {
+            return View();
+        }
+    }
+}
