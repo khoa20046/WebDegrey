abaf3fea6f67fca2676264116e5c46fefe7de555
khoa20046 <foxg0381@gmail.com>
2024-11-04 14:47:09 +0700

    Product, search item on product, fix adminlayout

--- DoAnLTW/Areas/Admin/Controllers/ProductsController.cs
+++ DoAnLTW/Areas/Admin/Controllers/ProductsController.cs
@@ -1,17 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using DoAnLTW.Models;
+using DoAnLTW.Models.ViewModel;
 
 namespace DoAnLTW.Areas.Admin.Controllers
 {
     public class ProductsController : Controller
     {
+        private MyStoreEntities db = new MyStoreEntities();
+
         // GET: Admin/Products
-        public ActionResult Index()
+        public ActionResult Index(string searchTerm, decimal? minPrice,
+            decimal? maxPrice, string sortOrder, int? page)
         {
+            var model = new ProductSearchVM();
+            var products = db.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(searchTerm))
+            { //Tim Kiếm sản phẩm dựa trên từ khóa
+                model.SearchTerm = searchTerm;
+                products = products.Where(p =>
+                p.ProductName.Contains(searchTerm) ||
+                p.ProductDescription.Contains(searchTerm) ||
+                p.Category.CategoryName.Contains(searchTerm));
+            }
+            //tìm kiếm sản phẩm dựa trên giá tối thiểu 
+            if (minPrice.HasValue)
+            {   
+                model.MinPrice = minPrice.Value;
+                products = products.Where(p => p.ProductPrice <= minPrice.Value);
+            }
+            //tìm kiếm sản phẩm dựa trên giá tối thiểu 
+            if (maxPrice.HasValue)
+            {
+                model.Maxprice = maxPrice.Value;
+                products = products.Where(p => p.ProductPrice <= maxPrice.Value);
+            }
+            //Áp dụng sắp xếp dựa trên lựa chọn của người dùng 
+            switch (sortOrder)
+            {
+                case "name_asc": products = products.OrderBy(p => p.ProductName); 
+                    break;
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.ProductName);
+                    break;
+                case "price_asc":
+                    products = products.OrderBy(p => p.ProductName);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(p => p.ProductName);
+                    break;
+            }
+            model.SortOrder = sortOrder;
+
+            model.Products = products.ToList();
+            return View(model);
+        }
+
+        // GET: Admin/Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Admin/Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
             return View();
+        }
+
+        // POST: Admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,CategoryID,ProductName,ProductDescription,ProductPrice,ProductImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // POST: Admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,CategoryID,ProductName,ProductDescription,ProductPrice,ProductImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
